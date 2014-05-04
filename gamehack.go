fa6b11067a03ecf4421c6c68475576313b6c8185
Zach Latta <zach@zachlatta.com>
2014-05-03 19:58:13 -0700

    Receive callback.

--- gamehack.go
+++ gamehack.go
@@ -1,12 +1,49 @@
 package gamehack
 
 import (
+	"encoding/json"
 	"fmt"
+	"io/ioutil"
 	"net/http"
 )
 
+type Notification struct {
+	UserID           int64             `json:"userId"`
+	StorylineUpdates []StorylineUpdate `json:"storylineUpdates"`
+}
+
+type StorylineUpdate struct {
+	// TODO: Change to equivalent of enum
+	Reason          string `json:"reason"`
+	LastSegmentType string `json:"lastSegmentType"`
+}
+
 func init() {
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprint(w, "Hello, world!")
-	})
+	http.HandleFunc("/notification", handleNotification)
+}
+
+func handleNotification(w http.ResponseWriter, r *http.Request) {
+	if r.Method != "POST" {
+		http.Error(w, "Invalid method.", http.StatusMethodNotAllowed)
+		return
+	}
+	defer r.Body.Close()
+	body, err := ioutil.ReadAll(r.Body)
+	if err != nil {
+		http.Error(w, "Error reading request body.", http.StatusBadRequest)
+		return
+	}
+
+	var notification Notification
+	err = json.Unmarshal(body, &notification)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	fmt.Fprintf(w, "%v", notification)
+	if err != nil {
+		http.Error(w, "Error writing response body.", http.StatusInternalServerError)
+		return
+	}
 }
