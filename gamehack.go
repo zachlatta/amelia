65217ac9db6fe1ddea1f201869bfef0113749e39
Andrew Downing <ad510@users.sourceforge.net>
2014-05-04 01:07:46 -0700

    add twilio function

--- gamehack.go
+++ gamehack.go
@@ -4,6 +4,10 @@ import (
 	"encoding/json"
 	"io/ioutil"
 	"net/http"
+	"fmt"
+	"github.com/subosito/twilio"
+	"appengine"
+	"appengine/urlfetch"
 )
 
 type Notification struct {
@@ -16,6 +20,22 @@ type StorylineUpdate struct {
 	Reason          string `json:"reason"`
 	LastSegmentType string `json:"lastSegmentType"`
 }
+
+type Location struct {
+	Lat float64 `json:"lat"`
+	Lon float64 `json:"lon"`
+}
+
+type Place struct {
+	Id int `json:"id"`
+	Type string `json:"type"`
+	Location Location `json:"location"`
+}
+
+var (
+	AccountSid = "AccountSid goes here"
+	AuthToken  = "AuthToken goes here"
+)
 
 func init() {
 	http.HandleFunc("/notification", handleNotification)
@@ -56,4 +76,31 @@ func handleNotification(w http.ResponseWriter, r *http.Request) {
 		http.Error(w, "Error writing response body.", http.StatusInternalServerError)
 		return
 	}*/
+	/*var place Place
+	err = json.Unmarshal(body, &place)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	sendText(place, "+15555555555", w, r)*/
+}
+
+func sendText(place Place, phone string, w http.ResponseWriter, r *http.Request) {
+	// Initialize twilio client
+	//c := twilio.NewClient(AccountSid, AuthToken, nil)
+
+	// You can set custom Client, eg: you're using `appengine/urlfetch` on Google's appengine
+	a := appengine.NewContext(r) // r is a *http.Request
+	f := urlfetch.Client(a)
+	c := twilio.NewClient(AccountSid, AuthToken, f)
+
+	// Send Message
+	params := twilio.MessageParams{
+		Body: fmt.Sprintf("Your child is now at lat %f lon %f", place.Location.Lat, place.Location.Lon),
+	}
+	_, _, err := c.Messages.Send("+15555555555", phone, params)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+	}
 }
